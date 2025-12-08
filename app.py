1391fb5b806a86a13d9865b8bd1967d2a1976a42
Ozan Güzel <150902284+ozanguzeell@users.noreply.github.com>
2025-12-08 03:35:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ with open('models/xgboost_model.pkl', 'rb') as f:
     model = pickle.load(f)
 
 st.title("🛒 Walmart Sales Forecasting App")
-
 st.markdown("Store, Dept ve bazı özellikleri girerek haftalık satış tahmini yapın.")
 
 store = st.number_input("Store", min_value=1, max_value=45, step=1)
@@ -18,9 +17,18 @@ month = st.number_input("Month", min_value=1, max_value=12)
 week = st.number_input("Week", min_value=1, max_value=52)
 isholiday = st.selectbox("Holiday?", [0, 1])
 
-data = pd.DataFrame([[store, dept, isholiday, week, year, month, size]],
-                    columns=['Store', 'Dept', 'IsHoliday', 'Week', 'Year', 'Month', 'Size'])
+data = pd.DataFrame({
+    'Store': [store],
+    'Dept': [dept],
+    'Size': [size],
+    'Year': [year],
+    'Month': [month],
+    'Week': [week],
+    'IsHoliday': [isholiday]
+})
 
+# XGBoost feature name validation kapatma
+model.get_booster().feature_names = None
 
 if st.button("Predict Sales"):
     prediction = model.predict(data)[0]
